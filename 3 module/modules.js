7ba2813dd5c8f994a53b9cc03ac3c4efd45c2b51
TRPMAN <trp.man08@gmail.com>
2025-04-21 21:59:03 +0700

    fix main

--- 3 module/modules.js
+++
@@ -1,6 +0,0 @@
-const { people, ages } = require('./people.js')
-
-console.log(people[0])
-
-const os = require('os')
-console.log(os.platform(), os.homedir())
--- 3 module/people.js
+++
@@ -1,7 +0,0 @@
-const people = ['John', 'Jane', 'Jim', 'Jill'] 
-const ages = [25, 30, 35, 40]
-
-module.exports = {
-    people,
-    ages
-}
